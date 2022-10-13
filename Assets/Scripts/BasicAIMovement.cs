95940559f5f69271a4dde60624351ae536dfc029
wanickols01 <wanickols@ualr.edu>
2022-10-13 00:45:23 -0500

    Random Movement
    Set up random movement based on a point generating system. Can easily put multiple bunnies on the map and the idle wandering around, and if they spot food they will targe it.

--- Assets/Scripts/BasicAIMovement.cs
+++ Assets/Scripts/BasicAIMovement.cs
@@ -14,14 +14,18 @@ public class BasicAIMovement : MonoBehaviour
     protected bool running; //mot sure if this wil stay here
 
     public NavMeshAgent agent;
+    public float movementRadius;
+
 
     // Start is called before the first frame update
     protected void Start()
     {
+       
+        agent = GetComponent<NavMeshAgent>();
         running = false;
         fov = GetComponent<FieldOfView>();
         fov.targetMask = LayerMask.GetMask("Food");
-
+        movementRadius = fov.viewRadius;
         fov.onTargetFound += Fov_onTargetFound;
     }
 
@@ -36,23 +40,42 @@ public class BasicAIMovement : MonoBehaviour
     {
         if (!running)
         {
-            Debug.Log("Not running! good. ");
             if (!target)
                 idleMovement();
             else
+            {
                 agent.SetDestination(target.position);
+            }
         }
         else 
         {
-            Debug.Log("Running. Bad!");
+            //Debug.Log("Running. Bad!");
         }
     }
 
     public void idleMovement() 
     {
-        Debug.Log("Idling. Bad!");
-        //move random direction lifelike
+
+        if (!agent.hasPath)
+        {
+            Debug.Log("Idling");
+            agent.SetDestination(GetPoints.Instance.GetRandomPoint(transform, movementRadius));
+        }
+        else 
+        {
+            Debug.Log(agent.steeringTarget);
+        }
     }
 
+
     //Predator Event overides everything
+#if UNITY_EDITOR
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, movementRadius);
+    }
+
+#endif
+
 }

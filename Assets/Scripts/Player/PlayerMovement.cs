af7e5e7ad9c187f8a5cf44395d4c2e575b22f35e
wanickols01 <wanickols@ualr.edu>
2022-12-11 19:49:46 -0600

    Update PlayerMovement.cs

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,29 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class PlayerMovement : MonoBehaviour
+using Unity.Netcode;
+public class PlayerMovement : NetworkBehaviour
 {
-    CharacterController characterController;
-    [SerializeField] float speed = 9.0f;
 
-    [SerializeField] private Vector3 moveDirection = Vector3.zero;
+    [SerializeField] private float movementSpeed = 7f;
+    [SerializeField] private float rotationSpeed = 400f;
+    [SerializeField] private Vector3 movement;
 
     private void Start()
     {
-        characterController = GetComponent<CharacterController>();
-        characterController.Move(moveDirection * speed * Time.deltaTime);
+        movement = new Vector3(0f,0f,0f);
     }
 
-    void Update()
+
+    private void Update()
     {
+        if (!IsOwner) return;
+
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
-        moveDirection = new Vector3(horizontal, 0.0f, vertical).normalized;
 
-        if (moveDirection.magnitude >= 0.1f) 
-           characterController.Move(moveDirection * speed * Time.deltaTime);
+        movement.Set(horizontal, 0f, vertical);
+
+        transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
+
+        if (movement != Vector3.zero) 
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
     }
+
+
+
 }
 
 
